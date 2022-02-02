1957fd1829d9305fa1c0b9aab84320fd9d61d213
valeriabonapersona <valeria.bonapersona@gmail.com>
2022-02-02 17:52:43 +0100

    main figure with only enough res

---
+++ src/figures/fig_males_main_res.R
@@ -0,0 +1,65 @@
+'
+  Script: Main results males at rest
+  Input: output of analysis_males.R
+  Output: Figure 4 paper
+  
+  Author: v.bonapersona-2@umcutrecht.nl
+
+'
+
+
+# Environment_preparation -------------------------------------------------
+source("src/analysis_males.R")
+
+
+# Dataset preparation -----------------------------------------------------
+males_mod_enough <- males_mod %>% 
+  filter(n_id > 2)
+
+
+# Visualization -----------------------------------------------------------
+fig_mono_main <- list()
+my_titles <- data.frame(
+  title = c("Dopamine system", "Serotonin system", 
+            "Noradrenaline system", "Monoaminergic enzymes"), 
+  out_mono = c("da_related", "ser_related", "ne_related", "enzymes")
+)
+for(i in unique(males_mod_enough$out_mono)) {
+  print(i)
+  
+  fig_mono_main[[i]] <- males_mod_enough %>%
+    filter(out_mono == i) %>%
+    mutate(
+      ba_grouped = str_replace_all(ba_grouped, "_", " \n"), 
+      my_col = ifelse(n_id > 2, "enough", "not_enough"), 
+      lab_sig = ifelse(n_id < 3, "", lab_sig)
+    ) %>%
+    
+    ggplot(aes(factor(ba_grouped, levels = ba_order), g), 
+           color = my_col) +
+    geom_bar(stat = "identity", aes(color = my_col), fill = "white") +
+    geom_errorbar(aes(ymin = g-sem, ymax = g+sem, color = my_col), width = 0.2) +
+    geom_hline(yintercept = 0, color = "black") +
+    
+    my_theme +
+    labs(x="", y = "Hedge's g") +
+    scale_color_manual(values = c("black", "grey75")) + 
+    
+    facet_grid(factor(outcome, levels = rev(order_all_out)) ~., scales = "free_y",
+               switch = "both") +
+    # 
+    # facet_grid(str_replace_all(outcome, "_", " \n")~., scales = "free_y",
+    #            switch = "both") +
+    labs(x = "Brain areas", y = "g(se)", title = my_titles[my_titles$out_mono == i,]$title) +
+    geom_text(aes(label = lab_sig, y = 0.1)) + 
+    theme(text = element_text(size = 20),
+          legend.position = "none") + 
+    theme(panel.spacing = unit(0.75, "lines")) +
+    theme(strip.background = element_rect(color = "white")) +       
+    theme(strip.text.y = element_text(size = 10, colour = "black",face = "bold")) 
+  
+  
+}
+
+
+
